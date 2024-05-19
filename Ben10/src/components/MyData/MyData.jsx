764f219057fae5ceb79cfe9f9c6c1ab148925c2f
TpSingh08 <s222197109@deakin.edu.au>
2024-05-20 09:49:29 +1000

    Commit10

---
+++ Ben10/src/components/MyData/MyData.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import { Navbar } from "../Navbar";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+export const MyData = () => {
+  return (
+    <div className="my-data">
+      <div className="div">
+        <div className="overlap">
+          <div className="text-wrapper">My Data</div>
+        </div>
+        <div className="overlap-group">
+          <div className="text-wrapper-2">My Data</div>
+          <img className="undraw-about-me-re" alt="Undraw about me re" src="/img/undraw-about-me-re-82bv-1.svg" />
+        </div>
+        <img className="line" alt="Line" src="/img/line-4.svg" />
+        <div className="div-wrapper">
+          {/* <div className="text-wrapper-3">Enter your name</div> */}
+          <input type="text" className="input-box" placeholder="Enter your name"/>
+        </div>
+        <div className="text-wrapper-4">Name:</div>
+        <div className="overlap-2">
+          {/* <div className="text-wrapper-3">Enter your weight</div> */}
+          <input type="text " className="input-box " placeholder="Enter your weight" />
+        </div>
+        <div className="text-wrapper-5">Weight:</div>
+        <div className="overlap-group-2">
+          {/* <div className="text-wrapper-3">Enter your sports</div> */}
+          <input type="text" className="input-box" placeholder="Enter your sports" />
+        </div>
+        <div className="text-wrapper-6">Sports:</div>
+        <div className="overlap-3">
+          {/* <p className="text-wrapper-3">Enter your team (if any)</p> */}
+          <input type="text" className="input-box" placeholder="Enter your team (if any)"/>
+        </div>
+        <div className="text-wrapper-7">Team:</div>
+        <div className="overlap-4">
+          {/* <div className="text-wrapper-3">Enter your age</div> */}
+          <input type="text" className="input-box" placeholder="Enter your age"/>
+        </div>
+        <div className="text-wrapper-8">Age:</div>
+        <div className="overlap-5">
+          {/* <div className="text-wrapper-3">Enter your height</div> */}
+          <input type="text" className="input-box" placeholder="Enter your height"/>
+        </div>
+        <div className="text-wrapper-9">Height:</div>
+        <div className="overlap-6">
+          <div className="text-wrapper-10">
+            <button className="text-wrapper-38">Save</button></div>
+        </div>
+        <div className="text-wrapper-11">Edit</div>
+        <img className="img" alt="Line" src="/img/line-1.svg" />
+        <p className="player-TRACKING">
+          <span className="span">Player </span>
+          <span className="text-wrapper-12">TRACKING</span>
+        </p>
+        <nav className="navbar">
+          <div className="navbar-container">
+        <Link className="text-wrapper-13 makeithover">Team Stats</Link>
+        <Link className="text-wrapper-14 makeithover">Player Stats</Link>
+        <Link to="/sign-up-page" className="text-wrapper-15 makeithover">Sign up</Link>
+        <Link to="/home-page" className="text-wrapper-16 makeithover">Home</Link>
+        </div></nav>
+        
+        <img className="microsoftteams-image" alt="Microsoftteams image" src="/img/microsoftteams-image-4.png" />
+        <p className="p">
+          <span className="span">Player </span>
+          <span className="text-wrapper-12">TRACKING</span>
+        </p>
+        <img className="microsoftteams-image-2" alt="Microsoftteams image" src="/img/microsoftteams-image-4.png" />
+        <div className="email-fake-email-com">
+          Email:
+          <br />
+          fake@email.com
+        </div>
+        <div className="phone-XXXX-XXX-XXX">
+          Phone:
+          <br />
+          XXXX-XXX-XXX
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyData;
---
+++ Ben10/src/components/SignUpPage/SignUpPage.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Navbar } from "../Navbar";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+export const SignUpPage = () => {
+  return (
+    <div className="sign-up-page">
+      <div className="div-3">
+        <div className="overlap-15">
+          <img className="undraw-greek-freak-j" alt="Undraw greek freak j" src="/img/undraw-greek-freak-j-2-qb-1.svg" />
+          <p className="player-TRACKING-4">
+            <span className="text-wrapper-35">Player </span>
+            <span className="text-wrapper-36">TRACKING</span>
+          </p>
+        </div>
+        <div className="text-wrapper-37">Welcome!</div>
+        <div className="overlap-group-4">
+          <Link to="/my-data">
+          <button className="text-wrapper-38">Sign Up</button></Link>
+        </div>
+        <div className="overlap-16">
+          <input className="input-field" placeholder="Enter your username" />
+        </div>
+        <div className="text-wrapper-40">Username</div>
+        <div className="overlap-17">
+          <input className="input-field" placeholder="Enter your email" />
+        </div>
+        <div className="text-wrapper-41">Email Id</div>
+        <div className="overlap-18">
+          <input type="password" className="input-field" placeholder="Enter your password" />
+        </div>
+        <div className="text-wrapper-42">Password</div>
+        <div className="text-wrapper-43">or</div>
+        <button className="text-wrapper-44 login-button">Log in</button>
+        <img className="line-6" alt="Line" src="/img/line-1-2.svg" />
+        <img className="microsoftteams-image-5" alt="Microsoftteams image" src="/img/microsoftteams-image-4.png" />
+      </div>
+    </div>
+  );
+};
+
+export default SignUpPage;
