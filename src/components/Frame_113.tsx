4dcbd1bf84db042162133052416439e74e105546
Mehak Beniwal <mehakbeniwal22@gmail.com>
2024-05-19 14:07:41 +0530

    this is our new code which has been integrated using React. Kindly Review it. But it still needs some changs to be more responsive this is a combined work of mine and mihir which we have mentioned in our files

---
+++ src/components/Frame_113.tsx
@@ -0,0 +1,60 @@
+//Done By Mehak Beniwal as well as the css for this//
+import styles from "./Frame_113.module.css";
+import image_28 from "./image_28.svg";
+import vector_1 from "./vector_1.svg";
+import vector_2 from "./vector_2.svg";
+import vector_3 from "./vector_3.svg";
+import vector_4 from "./vector_4.svg";
+import vector_5 from "./vector_5.svg";
+import vector_6 from "./vector_6.svg";
+import vector_7 from "./vector_7.svg";
+import whatsAppImage_2024_05_11At_15_44_3 from "./whatsAppImage_2024_05_11At_15_44_3.jpg";
+import vector_8 from "./vector_8.svg";
+import vector_9 from "./vector_9.svg";
+import image_29 from "./image_29.png";
+import image_31Image1 from "./image_31Image1.png";
+import image_42 from "./image_42.png";
+export const Frame_113 = ({ override }: { override?: React.CSSProperties }) => {
+  return (
+    <div className={styles.frame_113Component} style={override}>
+      <img className={styles.image_28} src={image_28.src} />
+      <img className={styles.vector_1} src={vector_1.src} />
+      <img className={styles.vector_2} src={vector_2.src} />
+      <img className={styles.vector_3} src={vector_3.src} />
+      <img className={styles.vector_4} src={vector_4.src} />
+      <img className={styles.vector_5} src={vector_5.src} />
+      <img className={styles.vector_6} src={vector_6.src} />
+      <img className={styles.vector_7} src={vector_7.src} />
+      <img
+        className={styles.whatsAppImage_2024_05_11At_15_44_3}
+        src={whatsAppImage_2024_05_11At_15_44_3.src}
+      />
+      <p className={styles.signInSignUp}>Sign - In Sign Up</p>
+      <p
+        className={
+          styles.aboutDashboardEventManagementResourcesAndSupportCrowdStatistics
+        }
+      >
+        About Dashboard Event Management Resources and Support Crowd Statistics
+      </p>
+      <img className={styles.vector_8} src={vector_8.src} />
+      <img className={styles.vector_9} src={vector_9.src} />
+      <img className={styles.image_29} src={image_29.src} />
+      <div className={styles.rectangle_13} />
+      <div className={styles.rectangle_14} />
+      <div className={styles.rectangle_14Div} />
+      <p className={styles.marketSupermarketRailwayStationMallsStadiumAirport}>
+        Market Supermarket Railway Station Malls Stadium Airport
+      </p>
+      <img className={styles.image_31} src={image_31Image1.src} />
+      <p className={styles.text450PeopleIdentifiedEntrance}>
+        450
+        <br />
+        People Identified
+        <br />
+        Entrance
+      </p>
+      <img className={styles.image_42} src={image_42.src} />
+    </div>
+  );
+};
---
+++ src/components/Frame_124.tsx
@@ -0,0 +1,53 @@
+//Done by Mehak and Mihir//
+import styles from "./Frame_124.module.css";
+import image_28Image from "./image_28Image.png";
+import vector_1 from "./vector_1.svg";
+import vector_2 from "./vector_2.svg";
+import vector_3 from "./vector_3.svg";
+import vector_4 from "./vector_4.svg";
+import vector_5 from "./vector_5.svg";
+import vector_6 from "./vector_6.svg";
+import vector_7 from "./vector_7.svg";
+import whatsAppImage_2024_05_11At_15_44_3 from "./whatsAppImage_2024_05_11At_15_44_3.jpg";
+import vector_8 from "./vector_8.svg";
+import vector_9 from "./vector_9.svg";
+import image_41 from "./image_41.png";
+export const Frame_124 = ({ override }: { override?: React.CSSProperties }) => {
+  return (
+    <div className={styles.frame_124Component} style={override}>
+      <div className={styles.frame_98} />
+      <div>
+        <div>
+          <div>
+            <img className={styles.image_28} src={image_28Image.src} />
+            <img className={styles.vector_1} src={vector_1.src} />
+            <img className={styles.vector_2} src={vector_2.src} />
+            <img className={styles.vector_3} src={vector_3.src} />
+            <img className={styles.vector_4} src={vector_4.src} />
+            <img className={styles.vector_5} src={vector_5.src} />
+            <img className={styles.vector_6} src={vector_6.src} />
+            <img className={styles.vector_7} src={vector_7.src} />
+            <img
+              className={styles.whatsAppImage_2024_05_11At_15_44_3}
+              src={whatsAppImage_2024_05_11At_15_44_3.src}
+            />
+            <p className={styles.signInSignUp}>Sign - In Sign Up</p>
+            <p
+              className={
+                styles.aboutDashboardEventManagementResourcesAndSupportCrowdStatistics
+              }
+            >
+              About Dashboard Event Management Resources and Support Crowd
+              Statistics
+            </p>
+            <img className={styles.vector_8} src={vector_8.src} />
+            <img className={styles.vector_9} src={vector_9.src} />
+            <div className={styles.image_29} />
+            <div className={styles.rectangle_13} />
+          </div>
+        </div>
+      </div>
+      <img className={styles.image_41} src={image_41.src} />
+    </div>
+  );
+};
